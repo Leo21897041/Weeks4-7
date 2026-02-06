5c7eff6bdf6379b4c4fab861ac0f26ba59e3b240
saporito <saporito@OA-S141-26>
2026-02-06 16:53:23 -0500

    The Week 5 In-Class commit, before the coding gym

---
+++ Assets/Scripts/Warper.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+public class Warper : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //IF WE CLICK AND WE ARENT HOVERING OVER UI
+        bool shouldWarp = Mouse.current.leftButton.wasPressedThisFrame && !EventSystem.current.IsPointerOverGameObject();
+
+        if (shouldWarp == true)
+        {
+            Vector3 currentMousePosition = Mouse.current.position.ReadValue();
+            transform.position = currentMousePosition;
+        }
+    }
+}
